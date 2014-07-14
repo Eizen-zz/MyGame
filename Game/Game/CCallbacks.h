44bbbdbebfc537a6f5c8fa266b70f1cc3eb1bf01
Андрей <mr.eizenhaim@yandex.ru>
2014-07-14 14:55:45 +0400

    Class callbacks

---
+++ Game/Game/CCallbacks.h
@@ -0,0 +1,18 @@
+#ifndef _CALLBACKS_H
+#define _CALLBACKS_H
+
+namespace Game
+{
+	class CCallbacks
+	{
+		public:
+			virtual void SpecialKeyboard(int Key, int x, int y) = 0;
+			virtual void Keyboard(unsigned char Key, int x, int y) = 0;
+			virtual void PassiveMouse(int x, int y) = 0;
+			virtual void Draw(void) = 0;
+			virtual void Idle(void) = 0;
+	};
+
+};
+
+#endif /* CALLBACKS_H */
