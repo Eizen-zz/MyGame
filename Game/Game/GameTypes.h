bc71c9157cf062a35141f5cccb35119b94a7b333
Андрей <mr.eizenhaim@yandex.ru>
2014-07-15 20:32:23 +0400

    Correction game types

--- Game/Game/GameTypes.h
+++ Game/Game/GameTypes.h
@@ -1,3 +1,12 @@
+#ifndef _GAME_TYPES_H
+#define _GAME_TYPES_H
+
 const float WindowWidth = 800;
 const float WindowHeight = 800;
+const float RacketWidth = (WindowWidth / 4) + (WindowWidth / 40);
+const float RacketHeight = (WindowHeight / 40) - (WindowHeight / 400);
 const int Time = 1;
+
+typedef unsigned int uint;
+
+#endif
